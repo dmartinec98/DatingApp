fa80b8d02e3ad5f4d3599fd3288148fa2a409c85
dmartinec <danijel.martinec98@gmail.com>
2020-11-10 14:07:56 +0100

    Angular,https,cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,10 +37,12 @@ namespace API
             });
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddCors();
+
+            /*services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            });*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,13 +51,16 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
+                /*app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                */
             }
 
             app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseRouting();            
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
